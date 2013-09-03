023828a3bca58930e8586719f9a5a272f7b18445
Zachary Marcantel <zmarcantel@utexas.edu>
2013-09-03 15:26:40 -0500

    Now adds functions to array prototype and checks validity

--- index.js
+++ index.js
@@ -20,174 +20,294 @@
   //
   //-----------------------------------------------------------------
 
-  eightbyte.and = function(a, b) {
-    return [(a[0] & b[0]) >>> 0, (a[1] & b[1]) >>> 0];
-  }
-
-  eightbyte.xor = function(a, b) {
-    return [(a[0] ^ b[0]) >>> 0, (a[1] ^ b[1]) >>> 0];
-  }
-
-  eightbyte.or = function(a, b) {
-    return [(a[0] | b[0]) >>> 0, (a[1] | b[1]) >>> 0];
-  }
-
-  eightbyte.rotl = function(number, bits) {
-    if (number[0] == 0 && number[1] == 0)
-      return number;
-
-    var high_shifted = (number[0] << bits) >>> 0,
-        high_rotated = (number[0] >>> (32 - bits)) >>> 0;
-
-    var low_shifted = (number[1] << bits) >>> 0,
-        low_rotated = (number[1] >>> (32 - bits)) >>> 0;
-
-    var high = (high_shifted | low_rotated) >>> 0,
-        low = (low_shifted | high_rotated) >>> 0;
-
-    return [high, low];
-  }
-
-  eightbyte.rotr = function(number, bits) {
-    if (number[0] == 0 && number[1] == 0)
-      return number;
-
-    var high_shifted = number[0] >>> bits,
-        high_rotated = (number[0] << (32 - bits)) >>> 0;
-
-    var low_shifted = number[1] >>> bits,
-        low_rotated = (number[1] << (32 - bits)) >>> 0;
-
-    var high = (high_shifted | low_rotated) >>> 0,
-        low = (low_shifted | high_rotated) >>> 0;
-
-    return [high, low];
-  }
-
-  eightbyte.shiftl = function(number, bits) {
-    var data = number;
-
-    if (number[0] == 0 && number[1] == 0)
-      return number;
-
-    if (bits == 64)
-      return [0, 0];
-
-    if (bits > 31) {
-      data[0] = data[1];
-      data[1] = 0;
-      bits -= 32;
+  //
+  // AND
+  //
+  Object.defineProperty(Array.prototype, 'and', {
+    value:function(b) {
+      if (!(this instanceof Array) || !(b instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+      return [(this[0] & b[0]) >>> 0, (this[1] & b[1]) >>> 0];
     }
+  });
 
-    var high_shifted = (data[0] << bits) >>> 0,
-        low_shifted = (data[1] << bits) >>> 0,
-        low_carry = (data[1] >>> (32 - bits)) >>> 0;
 
-    var high = (high_shifted | low_carry) >>> 0,
-        low = low_shifted;
-
-    return [high, low];
-  }
-
-  eightbyte.shiftr = function(number, bits) {
-    var data = number;
-
-    if (number[0] == 0 && number[1] == 0)
-      return number;
-
-    if (bits == 64)
-      return [0, 0];
-
-    if (bits > 31) {
-      data[1] = data[0] >>> 0;
-      data[0] = 0;
-      bits -= 32;
+  //
+  // XOR
+  //
+  Object.defineProperty(Array.prototype, 'xor', {
+    value:function(b) {
+      if (!(this instanceof Array) || !(b instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+      return [(this[0] ^ b[0]) >>> 0, (this[1] ^ b[1]) >>> 0];
     }
-
-    var high_shifted = (data[0] >>> bits) >>> 0,
-        low_shifted = (data[1] >>> bits) >>> 0,
-        high_carry = (data[0] << (32 - bits)) >>> 0;
-
-    var high = high_shifted,
-        low = (low_shifted | high_carry) >>> 0;
-
-    return [high, low];
-  }
+  });
 
 
-  eightbyte.add = function(a, b) {
-    if (a[0] == 0 && a[1] == 0) return b;
-    if (b[0] == 0 && b[1] == 0) return a;
-
-    var low = (a[1] + b[1]);
-    var carry = low > 0xFFFFFFFF ? (Math.floor(low / Math.pow(2,32)) >>> 0) : 0;
-    var high = (a[0] + b[0] + carry);
-
-    return [(high >>> 0), (low >>> 0)];
-  }
-
-
-  eightbyte.subtract = function(a, b) {
-    if (a[0] == 0 && a[1] == 0) return b;
-    if (b[0] == 0 && b[1] == 0) return a;
-
-    var high = a[0];
-    var low = (a[1] - b[1]);
-
-    if (low < 0) {
-      low = 0xFFFFFFFF - (low + high);
-      high = 0xFFFFFFFF - (high + low);
+  //
+  // OR
+  //
+  Object.defineProperty(Array.prototype, 'or', {
+    value: function(b) {
+      if (!(this instanceof Array) || !(b instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+      return [(this[0] | b[0]) >>> 0, (this[1] | b[1]) >>> 0];
     }
-
-    high -= b[0];
-
-    if (high < 0) high = 0;
-    if (low < 0) low = 0;
-
-    return [high, low];
-  }
+  });
 
 
-  eightbyte.multiply = function(a, b) {
-    a = [a[0] >>> 16, a[0] & 0xffff, a[1] >>> 16, a[1] & 0xffff];
-    b = [b[0] >>> 16, b[0] & 0xffff, b[1] >>> 16, b[1] & 0xffff];
-    var o = [0, 0, 0, 0];
+  //
+  // ROTL
+  //
+  Object.defineProperty(Array.prototype, 'rotl', {
+    value: function(bits) {
+      if (!(this instanceof Array) || this.length != 2) {
+        throw 'Malformed Array';
+      } else if (bits < 0) {
+        throw 'Cannot rotate negative bits.';
+      }
 
-    o[3] += a[3] * b[3];
-    o[2] += o[3] >>> 16;
-    o[3] &= 0xffff;
+      if (this[0] == 0 && this[1] == 0)
+        return this;
 
-    o[2] += a[2] * b[3];
-    o[1] += o[2] >>> 16;
-    o[2] &= 0xffff;
+      var high_shifted = (this[0] << bits) >>> 0,
+          high_rotated = (this[0] >>> (32 - bits)) >>> 0;
 
-    o[2] += a[3] * b[2];
-    o[1] += o[2] >>> 16;
-    o[2] &= 0xffff;
+      var low_shifted = (this[1] << bits) >>> 0,
+          low_rotated = (this[1] >>> (32 - bits)) >>> 0;
 
-    o[1] += a[1] * b[3];
-    o[0] += o[1] >>> 16;
-    o[1] &= 0xffff;
+      var high = (high_shifted | low_rotated) >>> 0,
+          low = (low_shifted | high_rotated) >>> 0;
 
-    o[1] += a[2] * b[2];
-    o[0] += o[1] >>> 16;
-    o[1] &= 0xffff;
+      return [high, low];
+    }
+  });
 
-    o[1] += a[3] * b[1];
-    o[0] += o[1] >>> 16;
-    o[1] &= 0xffff;
 
-    o[0] += (a[0] * b[3]) + (a[1] * b[2]) + (a[2] * b[1]) + (a[3] * b[0]);
-    o[0] &= 0xffff;
+  //
+  // ROTR
+  //
+  Object.defineProperty(Array.prototype, 'rotr', {
+    value: function(bits) {
+      if (!(this instanceof Array) || this.length != 2) {
+        throw 'Malformed Array';
+      } else if (bits < 0) {
+        throw 'Cannot rotate negative bits.';
+      }
 
-    o[0] = o[0] >>> 0;
-    o[1] = o[1] >>> 0;
-    o[2] = o[2] >>> 0;
-    o[3] = o[3] >>> 0;
+      if (this[0] == 0 && this[1] == 0)
+        return this;
 
-    var result = [(o[0] << 16) | o[1], (o[2] << 16) | o[3]];
-    return result;
-  }
+      var high_shifted = this[0] >>> bits,
+          high_rotated = (this[0] << (32 - bits)) >>> 0;
+
+      var low_shifted = this[1] >>> bits,
+          low_rotated = (this[1] << (32 - bits)) >>> 0;
+
+      var high = (high_shifted | low_rotated) >>> 0,
+          low = (low_shifted | high_rotated) >>> 0;
+
+      return [high, low];
+    }
+  });
+
+
+  //
+  // Shift Left
+  //
+  Object.defineProperty(Array.prototype, 'shiftl', {
+    value: function(bits) {
+      if (!(this instanceof Array) || this.length != 2) {
+        throw 'Malformed Array';
+      } else if (bits < 0) {
+        throw 'Cannot shift negative bits.';
+      }
+
+      var data = this;
+
+      if (this[0] == 0 && this[1] == 0)
+        return this;
+
+      if (bits == 64)
+        return [0, 0];
+
+      if (bits > 31) {
+        data[0] = data[1];
+        data[1] = 0;
+        bits -= 32;
+      }
+
+      var high_shifted = (data[0] << bits) >>> 0,
+          low_shifted = (data[1] << bits) >>> 0,
+          low_carry = (data[1] >>> (32 - bits)) >>> 0;
+
+      var high = (high_shifted | low_carry) >>> 0,
+          low = low_shifted;
+
+      return [high, low];
+    }
+  });
+
+
+  //
+  // Shift Right
+  //
+  Object.defineProperty(Array.prototype, 'shiftr', {
+    value: function(bits) {
+      if (!(this instanceof Array) || this.length != 2) {
+        throw 'Malformed Array';
+      } else if (bits < 0) {
+        throw 'Cannot shift negative bits.';
+      }
+
+      var data = this;
+
+      if (this[0] == 0 && this[1] == 0)
+        return this;
+
+      if (bits == 64)
+        return [0, 0];
+
+      if (bits > 31) {
+        data[1] = data[0] >>> 0;
+        data[0] = 0;
+        bits -= 32;
+      }
+
+      var high_shifted = (data[0] >>> bits) >>> 0,
+          low_shifted = (data[1] >>> bits) >>> 0,
+          high_carry = (data[0] << (32 - bits)) >>> 0;
+
+      var high = high_shifted,
+          low = (low_shifted | high_carry) >>> 0;
+
+      return [high, low];
+    }
+  });
+
+  //-----------------------------------------------------------------
+  //
+  // Mathematical Functions
+  //
+  //-----------------------------------------------------------------
+
+  //
+  // Add
+  //
+  Object.defineProperty(Array.prototype, 'add', {
+    value: function(b) {
+      if (!(this instanceof Array) || !(b instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+
+      if (this[0] == 0 && this[1] == 0) return b;
+      if (b[0] == 0 && b[1] == 0) return this;
+
+      var low = (this[1] + b[1]);
+      var carry = low > 0xFFFFFFFF ? (Math.floor(low / Math.pow(2,32)) >>> 0) : 0;
+      var high = (this[0] + b[0] + carry);
+
+      return [(high >>> 0), (low >>> 0)];
+    }
+  });
+
+
+  //
+  // Subtract
+  //
+  Object.defineProperty(Array.prototype, 'subtract', {
+    value: function(b) {
+      if (!(this instanceof Array) || !(b instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+
+      if (this[0] == 0 && this[1] == 0) return b;
+      if (b[0] == 0 && b[1] == 0) return this;
+
+      var high = this[0];
+      var low = (this[1] - b[1]);
+
+      if (low < 0) {
+        low = 0xFFFFFFFF - (low + high);
+        high = 0xFFFFFFFF - (high + low);
+      }
+
+      high -= b[0];
+
+      if (high < 0) high = 0;
+      if (low < 0) low = 0;
+
+      return [high, low];
+    }
+  });
+
+
+  //
+  // Multiply
+  //
+  Object.defineProperty(Array.prototype, 'multiply', {
+    value: function(b) {
+      if (!(this instanceof Array) || !(this instanceof Array) ||
+          this.length != 2 || b.length != 2) {
+        throw 'Malformed Array';
+      }
+
+      var a = [this[0] >>> 16, 
+           this[0] & 0xffff, 
+           this[1] >>> 16, 
+           this[1] & 0xffff];
+
+      b = [b[0] >>> 16,
+           b[0] & 0xffff,
+           b[1] >>> 16, 
+           b[1] & 0xffff];
+
+      var o = [0, 0, 0, 0];
+
+      o[3] += a[3] * b[3];
+      o[2] += o[3] >>> 16;
+      o[3] &= 0xffff;
+
+      o[2] += a[2] * b[3];
+      o[1] += o[2] >>> 16;
+      o[2] &= 0xffff;
+
+      o[2] += a[3] * b[2];
+      o[1] += o[2] >>> 16;
+      o[2] &= 0xffff;
+
+      o[1] += a[1] * b[3];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+
+      o[1] += a[2] * b[2];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+
+      o[1] += a[3] * b[1];
+      o[0] += o[1] >>> 16;
+      o[1] &= 0xffff;
+
+      o[0] += (a[0] * b[3]) + (a[1] * b[2]) + (a[2] * b[1]) + (a[3] * b[0]);
+      o[0] &= 0xffff;
+
+      o[0] = o[0] >>> 0;
+      o[1] = o[1] >>> 0;
+      o[2] = o[2] >>> 0;
+      o[3] = o[3] >>> 0;
+
+      var result = [(o[0] << 16) | o[1], (o[2] << 16) | o[3]];
+      return result;
+    }
+  });
 
   Object.defineProperty(Array.prototype, 'hex', {
     value: function() {
--- test/test.js
+++ test/test.js
@@ -24,65 +24,65 @@ describe('Creation', function() {
 describe('Binary Functions', function() {
   describe('AND', function() {
     it('0xAABBCCDD00000000 & 0xAABBCCDD00000000 -> 0xAABBCCDD00000000', function() {
-      var result = long.and([0xAABBCCDD, 0], [0xAABBCCDD, 0]);
+      var result = [0xAABBCCDD, 0].and([0xAABBCCDD, 0]);
       result.should.eql([0xAABBCCDD, 0]);
     });
 
     it('0xAABBCCDD00000000 & 0x00000000AABBCCDD -> 0x0000000000000000', function() {
-      var result = long.and([0xAABBCCDD, 0], [0, 0xAABBCCDD]);
+      var result = [0xAABBCCDD, 0].and([0, 0xAABBCCDD]);
       result.should.eql([0, 0]);
     });
 
     it('0xAABBCCDDEEFF0011 & 0x0F0F0F0F0F0F0F0F -> 0x0A0B0C0D0E0F0001', function() {
-      var result = long.and([0xAABBCCDD, 0xEEFF0011], [0x0F0F0F0F, 0x0F0F0F0F]);
+      var result = [0xAABBCCDD, 0xEEFF0011].and([0x0F0F0F0F, 0x0F0F0F0F]);
       result.should.eql([0x0A0B0C0D, 0x0E0F0001]);
     });
   });
 
   describe('OR', function() {
     it('0xAABBCCDDEEFF0011 OR 0x0F0F0F0F0F0F0F0F -> 0xAFBFCFDFEFFF0F1F', function() {
-      var result = long.or([0xAABBCCDD, 0xEEFF0011], [0x0F0F0F0F, 0x0F0F0F0F]);
+      var result = [0xAABBCCDD, 0xEEFF0011].or([0x0F0F0F0F, 0x0F0F0F0F]);
       result.should.eql([0xAFBFCFDF, 0xEFFF0F1F]);
     });
 
     it('0x0A0B0C0D0E0F0001 OR 0xA0B0C0D0E0F00010 -> 0xAABBCCDDEEFF0011', function() {
-      var result = long.or([0x0A0B0C0D, 0x0E0F0001], [0xA0B0C0D0, 0xE0F00010]);
+      var result = [0x0A0B0C0D, 0x0E0F0001].or([0xA0B0C0D0, 0xE0F00010]);
       result.should.eql([0xAABBCCDD, 0xEEFF0011]);
     });
   });
 
   describe('XOR', function() {
     it('0xAABBCCDDEEFF0011 XOR 0x0F0F0F0F0F0F0F0F -> 0xA5B4C3D2E1F00F1E', function() {
-      var result = long.xor([0xAABBCCDD, 0xEEFF0011], [0x0F0F0F0F, 0x0F0F0F0F]);
+      var result = [0xAABBCCDD, 0xEEFF0011].xor([0x0F0F0F0F, 0x0F0F0F0F]);
       result.should.eql([0xa5b4c3d2, 0xe1f00f1e]);
     });
 
     it('0xA0B0C0D0E0F00010 XOR 0x0F0F0F0F0F0F0F0F -> 0xAFBFCFDFEFFF0F1F', function() {
-      var result = long.xor([0xA0B0C0D0, 0xE0F00010], [0x0F0F0F0F, 0x0F0F0F0F]);
+      var result = [0xA0B0C0D0, 0xE0F00010].xor([0x0F0F0F0F, 0x0F0F0F0F]);
       result.should.eql([0xAFBFCFDF, 0xEFFF0F1F]);
     });
   });
 
   describe('Rotate Left', function() {
     it ('0x8000000000000000 rotl 1 -> 1', function() {
-      var result = long.rotl([0x80000000, 0], 1);
+      var result = [0x80000000, 0].rotl(1);
       result.should.eql([0, 1]);
     });
 
     it ('0xAABBCCDDEEFF0011 rotl 8 -> 0xBBCCDDEEFF0011AA', function() {
-      var result = long.rotl([0xAABBCCDD, 0xEEFF0011], 8);
+      var result = [0xAABBCCDD, 0xEEFF0011].rotl(8);
       result.should.eql([0xBBCCDDEE, 0xFF0011AA]);
     });
   })
 
   describe('Rotate Right', function() {
     it ('0x8000000000000000 rotr 1 -> 0', function() {
-      var result = long.rotr([0, 1], 1);
+      var result = [0, 1].rotr(1);
       result.should.eql([0x80000000, 0]);
     });
 
     it ('0xAABBCCDDEEFF0011 rotr 8 -> 0x11AABBCCDDEEFF00', function() {
-      var result = long.rotr([0xAABBCCDD, 0xEEFF0011], 8);
+      var result = [0xAABBCCDD, 0xEEFF0011].rotr(8);
       result.should.eql([0x11aabbcc, 0xddeeff00]);
     });
   })
@@ -90,69 +90,69 @@ describe('Binary Functions', function() {
 
   describe('Shift Left', function() {
     it ('0x8000000000000000 shiftl 1 -> 0', function() {
-      var result = long.shiftl([0x80000000, 0], 1);
+      var result = [0x80000000, 0].shiftl(1);
       result.should.eql([0, 0]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftl 8 -> 0xBBCCDDEEFF001100', function() {
-      var result = long.shiftl([0xAABBCCDD, 0xEEFF0011], 8);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftl(8);
       result.should.eql([0xBBCCDDEE, 0xFF001100]);
     });
 
     it ('0xAA shiftl 8 -> 0xAA00', function() {
-      var result = long.shiftl([0, 0xAA], 8);
+      var result = [0, 0xAA].shiftl(8);
       result.should.eql([0, 0xAA00]);
     });
 
     it ('0xAA shiftl 16 -> 0xAA0000', function() {
-      var result = long.shiftl([0, 0xAA], 16);
+      var result = [0, 0xAA].shiftl(16);
       result.should.eql([0, 0xAA0000]);
     });
 
     it ('0xAA shiftl 32 -> 0xAA00000000', function() {
-      var result = long.shiftl([0, 0xAA], 32);
+      var result = [0, 0xAA].shiftl(32);
       result.should.eql([0xAA, 0]);
     });
 
     it ('0xAA shiftl 64 -> 0', function() {
-      var result = long.shiftl([0, 0xAA], 64);
+      var result = [0, 0xAA].shiftl(64);
       result.should.eql([0, 0]);
     });
 
     it('0xEEFF0011 shiftl 16 -> 0x0000EEFF00110000', function() {
-      var result = long.shiftl([0, 0xEEFF0011], 16);
+      var result = [0, 0xEEFF0011].shiftl(16);
       result.should.eql([0x0000EEFF, 0x00110000]);
     });
   })
 
   describe('Shift Right', function() {
     it ('0x01 shiftr 1 -> 0', function() {
-      var result = long.shiftr([0, 1], 1);
+      var result = [0, 1].shiftr(1);
       result.should.eql([0, 0]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftr 8 -> 0x00AABBCCDDEEFF00', function() {
-      var result = long.shiftr([0xAABBCCDD, 0xEEFF0011], 8);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftr(8);
       result.should.eql([0x00AABBCC, 0xDDEEFF00]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftr 16 -> 0x0000AABBCCDDEEFF', function() {
-      var result = long.shiftr([0xAABBCCDD, 0xEEFF0011], 16);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftr(16);
       result.should.eql([0x0000AABB, 0xCCDDEEFF]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftr 32 -> 0x00000000AABBCCDD', function() {
-      var result = long.shiftr([0xAABBCCDD, 0xEEFF0011], 32);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftr(32);
       result.should.eql([0x00000000, 0xAABBCCDD]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftr 48 -> 0x000000000000AABB', function() {
-      var result = long.shiftr([0xAABBCCDD, 0xEEFF0011], 48);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftr(48);
       result.should.eql([0x00000000, 0x0000AABB]);
     });
 
     it ('0xAABBCCDDEEFF0011 shiftr 64 -> 0x0000000000000000', function() {
-      var result = long.shiftr([0xAABBCCDD, 0xEEFF0011], 64);
+      var result = [0xAABBCCDD, 0xEEFF0011].shiftr(64);
       result.should.eql([0x00000000, 0x00000000]);
     });
   })
@@ -160,32 +160,32 @@ describe('Binary Functions', function() {
 
   describe('Add', function() {
     it ('Simple1', function() {
-      var result = long.add([0xAABBCCDD, 0], [0, 0xEEFF0011]);
+      var result = [0xAABBCCDD, 0].add([0, 0xEEFF0011]);
       result.should.eql([0xAABBCCDD, 0xEEFF0011]);
     });
 
     it ('Simple2', function() {
-      var result = long.add([0x9bcced90, 0x4e4ee874], [0x0b46f7a4, 0xb2adfb7d]);
+      var result = [0x9bcced90, 0x4e4ee874].add([0x0b46f7a4, 0xb2adfb7d]);
       result.should.eql([0xa713e535, 0x00fce3f1]);
     });
 
     it ('Simple with Overflow', function() {
-      var result = long.add([0x3bdfcd16, 0x91e30f3f], [0xff11fd41, 0x5a3ea330]);
+      var result = [0x3bdfcd16, 0x91e30f3f].add([0xff11fd41, 0x5a3ea330]);
       result.should.eql([0x3af1ca57, 0xec21b26f]);
     });
 
     it ('Overflow on low: 1', function() {
-      var result = long.add([0, 0xFFFFFFFF], [0, 1]);
+      var result = [0, 0xFFFFFFFF].add([0, 1]);
       result.should.eql([1, 0]);
     });
 
     it ('Overflow on low: 0xFF', function() {
-      var result = long.add([0, 0xFFFFFFFF], [0, 0xFF]);
+      var result = [0, 0xFFFFFFFF].add([0, 0xFF]);
       result.should.eql([1, 0xFE]);
     });
 
     it ('Overflow on low: 0x100', function() {
-      var result = long.add([0, 0xFFFFFF00], [0, 0x1FF]);
+      var result = [0, 0xFFFFFF00].add([0, 0x1FF]);
       result.should.eql([1, 0xff]);
     });
   });
@@ -193,40 +193,40 @@ describe('Binary Functions', function() {
   
   describe('Subtract', function() {
     it('1 - 1 == 0', function() {
-      long.subtract([0, 1], [0, 1]).should.eql([0, 0]);
+      [0, 1].subtract([0, 1]).should.eql([0, 0]);
     });
 
     it('1 - 0 == 1', function() {
-      long.subtract([0, 1], [0, 0]).should.eql([0, 1]);
+      [0, 1].subtract([0, 0]).should.eql([0, 1]);
     });
  
     it('0x100000000 - 1 == 0xFFFFFFFF', function() {
-      long.subtract([0x1, 0x0], [0, 1])
+      [0x1, 0x0].subtract([0, 1])
           .should.eql([0, 0xFFFFFFFF]);
     });
 
     it('0x100000001 - 1 == 0x100000000', function() {
-      long.subtract([0x1, 0x1], [0, 1])
+      [0x1, 0x1].subtract([0, 1])
           .should.eql([1, 0]);
     });
 
     it('0xA00000000 - 0xA00000000 == 0x0', function() {
-      long.subtract([0xA, 0x0], [0xA, 0])
+      [0xA, 0x0].subtract([0xA, 0])
           .should.eql([0, 0]);
     });
 
     it('0xFFFFFFFFFFFFFFFF - 0xFFFFFFFFFFFFFFFF == 0x0', function() {
-      long.subtract([0xFFFFFFFF, 0xFFFFFFFF], [0xFFFFFFFF, 0xFFFFFFFF])
+      [0xFFFFFFFF, 0xFFFFFFFF].subtract([0xFFFFFFFF, 0xFFFFFFFF])
           .should.eql([0, 0]);
     });
 
     it('0xFFFFFFFFFFFFFFFF - 0xFFFFFFFFFFFFFFFE == 0x1', function() {
-      long.subtract([0xFFFFFFFF, 0xFFFFFFFF], [0xFFFFFFFF, 0xFFFFFFFE])
+      [0xFFFFFFFF, 0xFFFFFFFF].subtract([0xFFFFFFFF, 0xFFFFFFFE])
           .should.eql([0, 1]);
     });
 
     it('0xFFFFFFFFFFFFFFFF - 0xEEEEEEEEEEEEEEEE == 0x1111111111111111', function() {
-      long.subtract([0xFFFFFFFF, 0xFFFFFFFF], [0xEEEEEEEE, 0xEEEEEEEE])
+      [0xFFFFFFFF, 0xFFFFFFFF].subtract([0xEEEEEEEE, 0xEEEEEEEE])
           .should.eql([0x11111111, 0x11111111]);
     });
 
@@ -235,28 +235,28 @@ describe('Binary Functions', function() {
 
   describe('Multiply', function() {
     it ('Simple', function() {
-      var result = long.multiply([0, 0xFFFFFFFF], [0, 2]);
+      var result = [0, 0xFFFFFFFF].multiply([0, 2]);
       result.should.eql([1, 0xFFFFFFFE >> 0]);
     });
 
 
     it ('0xFFFFFFFF * 0xFFFFFFFF', function() {
-      var result = long.multiply([0, 0xFFFFFFFF], [0, 0xFFFFFFFF]);
+      var result = [0, 0xFFFFFFFF].multiply([0, 0xFFFFFFFF]);
       result.should.eql([0xfffffffe >> 0, 0x00000001]);
     });
 
     it ('0xAABBCCDD * 0xEEFF0011 :: negative signed', function() {
-      var result = long.multiply([0, 0xAABBCCDD], [0, 0xEEFF0011]);
+      var result = [0, 0xAABBCCDD].multiply([0, 0xEEFF0011]);
       result.should.eql([0x9f64a991 >> 0, 0xdc9b9aad >> 0]);
     });
 
     it ('0xAABBCCDD * 0x0011EEFF :: negative/positive', function() {
-      var result = long.multiply([0, 0xAABBCCDD], [0, 0x0011EEFF]);
+      var result = [0, 0xAABBCCDD].multiply([0, 0x0011EEFF]);
       result.should.eql([0xbf5dd, 0x44338623]);
     });
 
     it ('0x0011EEFF * 0xAABBCCDD :: commutative of above', function() {
-      var result = long.multiply([0, 0xAABBCCDD], [0, 0x0011EEFF]);
+      var result = [0, 0xAABBCCDD].multiply([0, 0x0011EEFF]);
       result.should.eql([0xbf5dd, 0x44338623]);
     });
   })
